aaf68cbd7a127339387427248301e29e379538bb
Amber Frantz <amber.k.frantz@mgail.com>
2022-08-28 09:24:00 -0400

    Completed Assignment

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.integer :taxi_id, null: false, foreign_key: true
+      t.integer :passenger_id, null: false, foreign_key: true
       t.timestamps
     end
   end
